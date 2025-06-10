a25843967147b92e67a54fd02727ad01b79af638
gkdudcc21 <gkdudcc21@gmail.com>
2025-06-10 13:54:03 +0900

    refactor: modularize RAG pipeline and AI message generation in llm.py

--- llm.py
+++ llm.py
@@ -8,18 +8,22 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
 from pinecone import Pinecone
 
+load_dotenv()
 
-## [AI Message 함수 정의] ##############################################################
-def get_ai_message(user_message):
-    ## 환경변수 읽어오기
-    load_dotenv()
+
+## llm 함수 정의 =======================================================================
+def get_llm(model='gpt-4o'):
+    llm = ChatOpenAI(model=model)
+    return llm
+
+
+## database 함수 정의 ==================================================================
+def get_database():
     PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
-    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
 
-    ## 임베딩 -> 백터 스토어(데이터베이스)에 저장
     ## 임베딩 모델 지정
     embedding = OpenAIEmbeddings(model="text-embedding-3-large")
-    pc = Pinecone(api_key=PINECONE_API_KEY)
+    Pinecone(api_key=PINECONE_API_KEY)
     index_name = 'law'
 
     database = PineconeVectorStore.from_existing_index(
@@ -27,9 +31,21 @@ def get_ai_message(user_message):
         embedding=embedding,
     )
 
-    ## RetrievalQA
-    llm = ChatOpenAI(model='gpt-4o')
-    prompt = hub.pull('rlm/rag-prompt')
+    return database
+
+
+## retrievalQA 함수 정의 ===============================================================
+def get_retrievalQA(): 
+    LANGCHAIN_API_KEY = os.getenv('LANGCHAIN_API_KEY')
+
+    ## vector store에서 index 정보
+    database = get_database()
+
+    ## prompt
+    prompt = hub.pull('rlm/rag-prompt', api_key=LANGCHAIN_API_KEY)
+    
+    ## LLM 모델 
+    llm = get_llm()
 
     def format_docs(docs):
         return "\n\n".join(doc.page_content for doc in docs)
@@ -44,5 +60,12 @@ def get_ai_message(user_message):
         | StrOutputParser()
     )
 
+    return qa_chain
+
+## [AI Message 함수 정의] ===============================================================
+def get_ai_message(user_message):
+
+    qa_chain = get_retrievalQA()
     ai_message = qa_chain.invoke(user_message)
+
     return ai_message
